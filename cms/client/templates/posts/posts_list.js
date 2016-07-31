d30e0069a10bd4a51942e7a3176f7d6ef991f85a
spacexu <spacexu@2bai.com.cn>
2016-07-31 14:07:33 +0800

    list2# meteor集合

--- cms/client/templates/posts/posts_list.js
+++ cms/client/templates/posts/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [
-	{
-		title: 'Discover Meteor',
-		url: 'http://wiki.jikexueyuan.com/project/discover-meteor/'
-	},
-	{
-		title: 'Meteor Tutorial',
-		url: 'https://www.meteor.com/tutorials/blaze/creating-an-app'
-	},
-	{
-		title: 'Baidu',
-		url: 'http://www.baidu.com'
-	}
-];
-
 Template.postsList.helpers({
-	posts: postsData
+	posts: function(){
+		return Posts.find();
+	}
 });
